586bb27687528c6d04b30e8800800cc6f705e19a
wbwiltshire <wbwiltshire@hotmail.com>
2022-11-09 17:03:14 -0500

    Updates for Microsoft.Data.SqlClient vulnerability and upgrade to DotNet 5.0

--- RunRegressionTest.cmd
+++ RunRegressionTest.cmd
@@ -1,3 +1,3 @@
 dotnet build /p:Platform="Any CPU"
-dotnet vstest .\Regression.Test\bin\Debug\net5.0\Regression.Test.dll --logger:trx;LogFileName=TestResult.xml
+dotnet vstest .\Regression.Test\bin\Debug\net6.0\Regression.Test.dll --logger:trx;LogFileName=TestResult.xml
 pause
--- SQL/ReloadDatabase.cmd
+++ SQL/ReloadDatabase.cmd
@@ -1,15 +1,15 @@
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i DropForeignKeyConstraints.sql
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i DropTables.sql
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateState.sql
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateCity.sql
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateContact.sql
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateProject.sql
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateProjectContact.sql
-SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateForeignKeyConstraints.sql
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i DropForeignKeyConstraints.sql -U dbuser -P P@ssw0rd
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i DropTables.sql -U dbuser -P P@ssw0rd
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateState.sql -U dbuser -P P@ssw0rd
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateCity.sql -U dbuser -P P@ssw0rd
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateContact.sql -U dbuser -P P@ssw0rd
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateProject.sql -U dbuser -P P@ssw0rd
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateProjectContact.sql -U dbuser -P P@ssw0rd
+SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i CreateForeignKeyConstraints.sql -U dbuser -P P@ssw0rd
 REM SQLCMD -S SCHVW2K12R2-DB\MSSQL2016 -i GrantStoredProcedureExecutePermissions.sql
-bcp DCustomer.dbo.State in backup\State.txt -c -t "|"  -T -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log
-bcp DCustomer.dbo.City in backup\City.txt -c -t "|"  -T -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log
-bcp DCustomer.dbo.Contact in backup\Contact.txt -c -t "|"  -T -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log
-bcp DCustomer.dbo.Project in backup\Project.txt -c -t "|"  -T -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log
-bcp DCustomer.dbo.ProjectContact in backup\ProjectContact.txt -c -t "|"  -T -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log
+bcp DCustomer.dbo.State in backup\State.txt -c -t "|"  -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log -U dbuser -P P@ssw0rd
+bcp DCustomer.dbo.City in backup\City.txt -c -t "|" -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log -U dbuser -P P@ssw0rd
+bcp DCustomer.dbo.Contact in backup\Contact.txt -c -t "|" -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log -U dbuser -P P@ssw0rd
+bcp DCustomer.dbo.Project in backup\Project.txt -c -t "|" -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log -U dbuser -P P@ssw0rd
+bcp DCustomer.dbo.ProjectContact in backup\ProjectContact.txt -c -t "|" -S SCHVW2K12R2-DB\MSSQL2016 -e bcp.log -U dbuser -P P@ssw0rd
 Pause
